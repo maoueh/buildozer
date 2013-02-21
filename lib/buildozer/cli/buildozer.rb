1f21ec1d80f16b38adbb424441fb2208531c5d7a
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2013-02-20 21:37:56 -0500

    Improved the rpm task  * Added an option to pass directory where package files are located  * Now building the rpm when invoked

--- lib/buildozer/cli/buildozer.rb
+++ lib/buildozer/cli/buildozer.rb
@@ -7,12 +7,13 @@ module Buildozer
     class Buildozer < Thor
 
       desc "rpm FILE", "Build a rpm package using fpm"
+      method_option :dir, :aliases => "-d", :default => ".", :desc => "Directory where package files are located"
       def rpm(file)
         definition = Dsl.compile(file)
 
         definition.packages.each do |package|
-          builder = Builder::Rpm.new(package,  "/tmp")
-          puts builder.command()
+          builder = Builder::Rpm.new(package, File.expand_path(options[:dir]))
+          puts builder.build()
         end
       end
     end
